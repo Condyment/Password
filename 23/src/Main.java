8a60bb427bd448b626ee7c2f03803c7665f27731
Marton <Marton@Marton-PC>
2017-12-19 01:44:11 +0200

    water

--- 23/src/Main.java
+++ 23/src/Main.java
@@ -1,9 +1,11 @@
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
 	public static void main(String[] args) {
 
 		try {
@@ -11,21 +13,34 @@ public class Main {
 			FileInputStream fileInStreamObj = new FileInputStream("parole.txt");
 			InputStream inStreamObject = ((InputStream) fileInStreamObj);
 			Scanner sc = new Scanner(inStreamObject);
+			FileInputStream fileInStreamObj1 = new FileInputStream("parole.txt");
+			InputStream inStreamObject1 = ((InputStream) fileInStreamObj1);
+			Scanner sc1 = new Scanner(inStreamObject1);
 			Scanner s = new Scanner(System.in);
+			int numberoflines = 0;
+			while (sc1.hasNextLine()) {
 
-			String input = sc.nextLine();
-			String input1 = sc.nextLine();
-			String input2 = sc.nextLine();
+				sc1.nextLine();
+				numberoflines++;
+			}
 
-			String[] arr = input.split(" ");
-			String[] arr1 = input1.split(" ");
-			String[] arr2 = input2.split(" ");
-			String[] usernames = { arr[0], arr1[0], arr2[0] };
-			String[] parole = { arr[1], arr1[1], arr2[1] };
+			String ppp[] = new String[numberoflines];
+			for (int i = 0; i < numberoflines; i++) {
+				String input = sc.nextLine();
+				ppp[i] = input;
+			}
+			String usernames[] = new String[numberoflines];
+			String parole[] = new String[numberoflines];
+			for (int i = 0; i < numberoflines; i++) {
+				String arr[] = ppp[i].split(" ");
+
+				usernames[i] = arr[0];
+				parole[i] = arr[1];
+			}
 
 			int k = 0;
-			int pp = 0;
 			int zz = 0;
+			int pp = 0;
 			while (k <= 15) {
 				System.out.println("Enter user name");
 				String user = s.nextLine();
@@ -34,52 +49,55 @@ public class Main {
 				String pass = s.nextLine();
 
 				int i = 0;
-				for (i = 0; i < usernames.length; i++) {
+				for (i = 0; i < numberoflines; i++) {
 					if (user.equals(usernames[i])) {
 
 						if (pass.equals(parole[i])) {
 							System.out.println("acces permis");
 							k = 16;
-							pp = 0;
-							zz = 0;
-						} else {
-							zz = zz + 5;
 
+						} else {
+
+							zz = zz + 3;
 						}
-						pp = 0;
+
 					} else {
 
-						pp = pp + 5;
+						pp = pp + 1;
 
 					}
 
 				}
-				System.out.println(zz);
-				System.out.println(pp);
-				if (zz >= 15) {
+
+				if (zz >= 9) {
 
 					System.out.println("Cont Blocat");
-					k=16;
+					k = 16;
+				} else {
+					System.out.println(zz);
 				}
-				if (pp >= 75) {
+				if (pp >= 5 * numberoflines) {
 
 					System.out.println("Ar trebui sa inregistrezi");
-					FileOutputStream fout=new FileOutputStream("parole.txt",true);    
+					FileOutputStream fout = new FileOutputStream("parole.txt", true);
 					System.out.println("Enter  new user name");
-					 user = s.nextLine();
-					 byte a[]=user.getBytes();
-					 fout.write(13);
-					 fout.write(a);
-					 fout.write(32);
+					user = s.nextLine();
+					byte a[] = user.getBytes();
+					fout.write(13);
+					fout.write(a);
+					fout.write(32);
 					System.out.println("Enter  new user pass");
-					 pass = s.nextLine();
-		             byte b[]=pass.getBytes();
-		             
-		             fout.write(b);
-		             fout.close();    
-		             System.out.println("success...");   
-		             k=16;
+					pass = s.nextLine();
+					byte b[] = pass.getBytes();
+
+					fout.write(b);
+					fout.close();
+					System.out.println("success...");
+					k = 16;
+				} else {
+					System.out.println(pp);
 				}
+
 			}
 
 		} catch (Exception e) {
@@ -87,4 +105,5 @@ public class Main {
 		}
 
 	}
+
 }
